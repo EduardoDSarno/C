c5fdc729d0db54e9481dd5def6eb084855f288ba
eduardosarno <eduartesarno@gmail.com>
2026-04-04 18:44:47 -0700

    watermelon

---
+++ DSA/CodeForces/watermelon_A_800.cpp
@@ -0,0 +1,51 @@
+// g++ -std=c++17 -DLOCAL -o test test.cpp && ./test
+
+/* Set up*/
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+void watermelon(int x);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+    #endif
+    
+    // code
+    int test;
+
+    cin >> test;
+    watermelon(test);
+
+    // read first line
+    // for(int i = 0; i < tests ;++i){
+
+       
+    // }
+
+    
+}
+
+void watermelon(int x){
+
+    if (x <= 2) {
+        cout << "NO";
+        return;
+    }
+    (x % 2 == 0 && x > 2) ? cout << "YES" : cout << "NO";
+}
