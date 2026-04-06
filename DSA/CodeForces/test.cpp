ad2cc72993853e038a320705fb6994a8d1621b74
eduardosarno <eduartesarno@gmail.com>
2026-04-06 13:51:15 -0700

    theater square 1000, this was a little bit challenging already

--- DSA/CodeForces/test.cpp
+++ DSA/CodeForces/test.cpp
@@ -1,17 +1,22 @@
 // g++ -std=c++17 -DLOCAL -o test test.cpp && ./test
 
 /* Set up*/
-#include <cctype>
 #include <cstdio>
-#include <cstring>
-#include <ios>
+#include <fstream>
 #include <iostream>
+#include <ostream>
+#include <vector>
+#include <algorithm>
 #include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
 
 using namespace std;
-
-void capi(string s);
-
+void countingStones(int n, int m, int a);
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -21,23 +26,32 @@ int main() {
     #endif
     
     // code
-    string s;
+    int n,m,a;
 
-    cin >> s;
+    cin >> n;
+    cin >> m;
+    cin >> a;
 
-    // read first line
-    capi(s);
-}
-
-void capi(string s){
-
-    int i = 0;
-    while (!isalpha(s[i])) 
-    {
-        i++;
-    }
-    s[i] = toupper(s[i]);
-    cout << s << '\n';
+    countingStones(n, m, a);
     
 }
+
+void countingStones(int n, int m, int a){
+
+
+    int area = n * m;
+
+    int area_stone = a*a;
+
+    int num_stones_width  = (n + a - 1) / a; // + 1 t round up
+    int num_stones_height = (m + a - 1) / a;
+
+
+    int total_stones = num_stones_width * num_stones_height;
+
+    cout << total_stones << '\n';
+
+
+}
+
 
--- DSA/CodeForces/theater_square_A.cpp
+++ DSA/CodeForces/theater_square_A.cpp
@@ -16,7 +16,7 @@
 #include <climits>
 
 using namespace std;
-
+void countingStones(long long n, long long m, long long a);
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -26,35 +26,32 @@ int main() {
     #endif
     
     // code
-    int n,m,a;
+    long long n,m,a;
 
     cin >> n;
     cin >> m;
     cin >> a;
+
+    countingStones(n, m, a);
     
 }
 
-void countingStones(int n, int m, int a){
+void countingStones(long long n, long long m, long long a){
 
 
-    int area = n * m;
+    long long area = n * m;
 
-    int area_stone = a*a;
+    long long area_stone = a*a;
 
-    int count = 0;
-    // if they fit nicely
-    if(area % area_stone == 0){
+    long long num_stones_width  = (n + a - 1) / a; // + 1 t round up
+    long long num_stones_height = (m + a - 1) / a;
 
-        while (area != 0) {
-            
-            area -= area_stone;
-            count++;
-        }
-       
-    }
-    
 
+    long long total_stones = num_stones_width * num_stones_height;
+
+    cout << total_stones << '\n';
 
 
 }
+
 
